e6664fad7404c577c426fda1ded3da3f642f5128
madmagic007 <51405665+madmagic007@users.noreply.github.com>
2023-02-24 23:01:30 +0100

    Update examplemodule.lua

--- examplemodule.lua
+++ examplemodule.lua
@@ -2,7 +2,7 @@ local module = {
     appName = "Code",
     titleContains = "Visual",
     discordAppID = "642057918921048075",
-    updateUrl = "https://raw.githubusercontent.com/madmagic007/EverythingRichPresence/main/examplemodule.lua?token=GHSAT0AAAAAAB3YYQFG3JL42BVPCD23GPKKY7YUNHA"
+    updateUrl = "https://raw.githubusercontent.com/madmagic007/EverythingRichPresence/main/examplemodule.lua"
 }
 
 RegisterModule(module, function()
