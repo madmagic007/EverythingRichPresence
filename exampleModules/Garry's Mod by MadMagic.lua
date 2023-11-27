6f366568bc6bcb775c70935732a5e9a6b073321c
madmagic007 <diegogo@telenet.be>
2023-11-27 15:52:10 +0100

    edit gitignore

---
+++ exampleModules/Garry's Mod by MadMagic.lua
@@ -0,0 +1,43 @@
+local module = {
+    discordAppId = "772853901396279376",
+    appName = "gmod",
+    titleContains = "x64",
+    updateUrl = "https://raw.githubusercontent.com/madmagic007/EverythingRichPresence/main/exampleModules/Garry's%20Mod%20x64%20by%20MadMagic.lua"
+}
+
+local addresses = {
+    serverName = "client.dll+7B4F20",
+    map = "engine.dll+4F3F80",
+    gamemode = "client.dll+6F5898"
+}
+
+local gamemodeMapping = {
+    prop_hunt = "Prop Hunt",
+    terrortown = "TTT (Trouble in Terrorist Town)"
+}
+
+RegisterModule(module, function()
+    local serverName = Mem.readString(addresses.serverName)
+    local gamemode =  Mem.readString(addresses.gamemode)
+    local map = Mem.readString(addresses.map)
+
+    local presence = {
+        largeImageKey = "gmod",
+        largeImageText = "Garry's Mod RPC By MadMagic",
+        details = "In Main Menu"
+    }
+
+    if map ~= "" then
+        local niceGamemode = gamemodeMapping[gamemode]
+        if niceGamemode == nil then
+            niceGamemode = gamemode
+        end
+
+        presence.details = "Playing on " .. serverName
+        presence.state = "Playing " .. niceGamemode .. " on " .. map
+        presence.smallImageKey = gamemode
+        presence.smallImageText = niceGamemode
+    end
+    
+    SetPresence(presence)
+end)
